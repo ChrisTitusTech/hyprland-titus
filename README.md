4b2269437ef3c8124719fd4c5fba6dfa2fd13b21
ChrisTitusTech <dfm.titus@gmail.com>
2023-01-09 20:28:57 -0600

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Hyprland config files from Titus
+
+
+## Install
+
+### Yay
+
+Run as user NOT ROOT!
+
+```
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si
+```
+
+### Packages
+
+```
+yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior dunst rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock
+```
+
+## Sources used making these
+
+- Official Hyprland Github <https://github.com/hyprwm/Hyprland>
+- Linux Mobiles Hyprland dot files <https://github.com/linuxmobile/hyprland-dots>
---
+++ dotconfig/rofi/powermenu/powermenu.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+# Available Styles
+# >> Created and tested on : rofi 1.6.0-1
+#
+# column_circle     column_square     column_rounded     column_alt
+# card_circle     card_square     card_rounded     card_alt
+# dock_circle     dock_square     dock_rounded     dock_alt
+# drop_circle     drop_square     drop_rounded     drop_alt
+# full_circle     full_square     full_rounded     full_alt
+# row_circle      row_square      row_rounded      row_alt
+
+theme="drop_square"
+dir="$HOME/.config/rofi/powermenu"
+
+
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/$theme"
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+			systemctl poweroff
+        ;;
+    $reboot)
+			systemctl reboot
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+        ;;
+    $logout)
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+        ;;
+esac
