3540d10b018006078848d04b17767f0f5c670485
Chris Titus <contact@christitus.com>
2025-06-10 14:48:23 -0500

    update to latest titus hyprland config

--- set-hypr.sh
+++ set-hypr.sh
@@ -45,10 +45,9 @@ if [[ $inst =~ ^[Nn]$ ]]; then
         fi
 
 if [[ $inst =~ ^[Yy]$ ]]; then
-   git_pkgs="grimblast-git hyprpicker-git aylurs-gtk-shell"
-   hypr_pkgs="hyprland wl-clipboard wf-recorder rofi sddm wlogout dunst swww alacritty hyprcursor hyprlang noto-fonts noto-fonts-emoji"
-   app_pkgs="nwg-look qt5ct btop jq gvfs ffmpegthumbs mpv playerctl pamixer noise-suppression-for-voice"
-   app_pkgs2="mate-polkit ffmpeg neovim viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs"
+   hypr_pkgs="hyprland wl-clipboard wf-recorder dunst swww alacritty hyprcursor hyprlang noto-fonts noto-fonts-emoji xdg-desktop-portal-hyprland"
+   app_pkgs="nwg-look qt5ct btop jq gvfs ffmpegthumbs mpv playerctl fuzzel"
+   app_pkgs2="hyprpolkitagent ffmpeg neovim viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs ttf-meslo-nerd ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-roboto-mono-nerd ttf-ubuntu-mono-nerd"
    theme_pkgs="nordic-theme papirus-icon-theme starship"
 
     # Check for yay or paru
@@ -61,7 +60,7 @@ if [[ $inst =~ ^[Yy]$ ]]; then
         exit 1
     fi
 
-    if ! $aur_helper -S --noconfirm $git_pkgs $hypr_pkgs $app_pkgs $app_pkgs2 $theme_pkgs 2>&1 | tee -a $LOG; then
+    if ! $aur_helper -S --noconfirm $hypr_pkgs $app_pkgs $app_pkgs2 $theme_pkgs 2>&1 | tee -a $LOG; then
         print_error " Failed to install additional packages - please check the install.log \n"
         exit 1
     fi
@@ -79,64 +78,10 @@ fi
 read -n1 -rep "${CAT} Would you like to copy config files? (y,n)" CFG
 if [[ $CFG =~ ^[Yy]$ ]]; then
     printf " Copying config files...\n"
-    cp -r dotconfig/dunst ~/.config/ 2>&1 | tee -a $LOG
     cp -r dotconfig/hypr ~/.config/ 2>&1 | tee -a $LOG
-    cp -r dotconfig/kitty ~/.config/ 2>&1 | tee -a $LOG
-    cp -r dotconfig/pipewire ~/.config/ 2>&1 | tee -a $LOG
-    cp -r dotconfig/rofi ~/.config/ 2>&1 | tee -a $LOG
-    cp -r dotconfig/wlogout ~/.config/ 2>&1 | tee -a $LOG
-    
-    # Set some files as exacutable 
-    chmod +x ~/.config/hypr/xdg-portal-hyprland
-fi
-
-FONT_DIR="$HOME/.local/share/fonts"
-FONT_ZIP="$FONT_DIR/Meslo.zip"
-FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
-
-# Check if Meslo Nerd-font is already installed
-if fc-list | grep -qi "Meslo"; then
-    echo "Meslo Nerd-fonts are already installed."
-    exit 0
-fi
-
-echo "Installing Meslo Nerd-fonts..."
-
-# Create the fonts directory if it doesn't exist
-mkdir -p "$FONT_DIR"
-
-# Download the font zip file if it doesn't already exist
-if [ ! -f "$FONT_ZIP" ]; then
-    wget -O "$FONT_ZIP" "$FONT_URL" || {
-        echo "Failed to download Meslo Nerd-fonts from $FONT_URL"
-        exit 1
-    }
-else
-    echo "Meslo.zip already exists in $FONT_DIR, skipping download."
-fi
-
-if [ ! -d "$FONT_DIR/Meslo" ]; then
-    unzip -o "$FONT_ZIP" -d "$FONT_DIR" || {
-        echo "Failed to unzip $FONT_ZIP"
-        exit 1
-    }
-else
-    echo "Meslo font files already unzipped in $FONT_DIR, skipping unzip."
-fi
-rm "$FONT_ZIP"
-fc-cache -fv # Rebuild the font cache
-echo "Meslo Nerd-fonts installed successfully"
-
-### Enable SDDM Autologin ###
-read -n1 -rep 'Would you like to enable SDDM autologin? (y,n)' SDDM
-if [[ $SDDM == "Y" || $SDDM == "y" ]]; then
-    LOC="/etc/sddm.conf"
-    echo -e "The following has been added to $LOC.\n"
-    echo -e "[Autologin]\nUser = $(whoami)\nSession=hyprland" | sudo tee -a $LOC
-    echo -e "\n"
-    echo -e "Enabling SDDM service...\n"
-    sudo systemctl enable sddm
-    sleep 3
+    cp -r dotconfig/fuzzel ~/.config/ 2>&1 | tee -a $LOG
+    cd ~/.config
+    git clone https://github.com/christitustech/quickshell.git 2>&1 | tee -a $LOG
 fi
 
 # BLUETOOTH
